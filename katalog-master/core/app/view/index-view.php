940e0a74aaa4e6c2ea481bbb7044ff448c89cd0f
aveliz15 <aveliz15@hotmail.com>
2019-11-17 22:36:56 -0600

    Se agregó el index, se cambiaron las leyendas del footer, pendiente arreglar index

---
+++ katalog-master/core/app/view/index-view.php
@@ -0,0 +1,188 @@
+<?php 
+$cnt=0;
+$slides = SlideData::getPublics();
+$featureds = PostData::getFeatureds();
+?>
+<section>
+  <div class="container">
+
+  <div class="row">
+
+  <div class="col-md-12">
+<!-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+    
+<br>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Prueba bootsrap 4</title>
+  <meta charset="utf-8">
+  <meta name="viewport"  content="width=device/width, initial-scale=1, shrink-to-fit=no"></meta>
+  <meta http-equiv="x-ua-compatible" content="ie-edge">
+
+  <!--<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"> -->
+
+    <style>
+.parallax {
+  /* The image used */
+  background-image: url("img/panelHome.jpg");
+
+  /* Set a specific height */
+  height: 500px;
+
+  /* Create the parallax scrolling effect */
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
+
+</head>
+
+
+
+
+<body>
+
+
+
+
+<div class="container"><!-- Bloque de menu -->
+
+  
+  
+</div><!-- Bloque de menu final -->
+
+
+<div class="container-fluid parallax" ><!-- Bloque 1 -->
+  <div class="row pt-5">
+    <div class="col-12 col-md-2 bg-dark d-none d-sm-none d-md-block"></div>
+    <div class="col-12 col-md-4 bg-light align-self-center pl-5">
+      
+      <h3>SUNLIGHT</h3>
+      <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      
+
+      </p>
+
+    </div>
+    
+    <!--<div class="col-12 col-md-6 d-none d-sm-none d-md-block justify-content-center parallax ">
+      
+    <img src="img/gym2.jpg" class="rounded img-fluid">
+
+    </div> -->
+</div>
+</div> <!-- Bloque 1 Final-->
+
+<div class="container border-left border-right "> <!-- Bloque 2 -->
+  <div class="row pt-5 pb-5 ">
+    <div class="col-12 col-md-6  justify-content-center">
+      <img src="img/panelTitulo1.jpg" class="rounded img-fluid ">
+      
+    </div>
+
+    <div class="col-12 col-md-4 bg-light align-self-center pl-5">
+            <a name="horario"><h3>Titulo</h3></a>
+            
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, sunt dolores necessitatibus ipsum est ut dignissimos esse accusantium sint, quasi repellendus, sed itaque asperiores tenetur consequatur voluptates neque at doloribus. </p>
+    <!--  <p>Lunes a viernes <br>
+      6:00 Am a 9:00 Pm <br><br>
+
+      Sabado <br>
+      6:00 Am a 4:00 Pm <br><br>
+
+      Domingo <br>
+      9:00 Am a 2:00 Pm <br><br><br>
+
+      </p>
+-->
+
+    </div>
+    <div class="col-12 col-md-2 bg-dark d-none d-sm-none d-md-block">
+      
+
+
+    </div>
+
+  </div>
+  <!-- Bloque 2 final -->
+
+  <div class="row pt-5"><!-- Bloque 3 -->
+    <div class="col-12 col-md-2 bg-dark d-none d-sm-none d-md-block"></div>
+    <div class="col-12 col-md-4 bg-light align-self-center pl-5">
+      
+      <a name="ubicacion"><h3>Titulo</h3></a>
+      <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+      </p>
+
+    </div>
+    
+    <div class="col-12 col-md-6 justify-content-center ">
+      
+    <img src="img/panelTitulo2.jpg" class="rounded img-fluid">
+
+    </div>
+
+<!-- Bloque 3 final -->
+
+  
+
+
+
+<!-- Footer -->
+<div class="container border-top border-left bo">
+<div class="row">
+  <div class="col-8  pt-3">
+    <h6>Telefonos:</h6><p> 1234-1234 <br>7894-7894</p>
+  </div>
+  <div class="col-4  pt-3">
+    <h6>Redes sociales</h6><p>Facebook instagram</p>
+  </div>
+</div>
+</div>
+
+<!-- footer final -->
+
+
+
+
+
+
+
+
+
+</body>
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+
+</html>           
+
+
+
+
+
+  </div>
+  </section>
